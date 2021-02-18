3d9593deb4155d7db4f6383efe0dff69ca9597e2
Jo-Ting Tang <49953810+et31terry@users.noreply.github.com>
2021-02-18 12:02:45 +0800

    Add files via upload

---
+++ common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer'] 	= '支持<a href="https://sufu.com.tw">蔬福良品</a> & <a href="https://supremelody.com.tw">光量生物科技</a> & <a href="https://vegeden.com.tw">蔬食伊甸園</a>&copy; ' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] 	= '版本 %s';
